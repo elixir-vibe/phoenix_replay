5d38aa63867f9b0ceed137f52f3541d59e653f62
Danila Poyarkov <dev@dannote.net>
2026-06-06 16:59:40 +0300

    Use Elixir 1.20 by default

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule PhoenixReplay.MixProject do
     [
       app: :phoenix_replay,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
